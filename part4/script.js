78bbf7f943da7d76964e83e24ba4e3ae9e62abd8
ChunChunMaru <93242480+ChunChunMaruOfficial@users.noreply.github.com>
2025-02-27 23:56:41 +0300

    task 4 done:3

--- part4/script.js
+++ part4/script.js
@@ -22,6 +22,7 @@ var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
     }
     return to.concat(ar || Array.prototype.slice.call(from));
 };
+var _a, _b, _c, _d, _e, _f;
 var getrandomInit = function (min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 };
@@ -52,6 +53,13 @@ var HealthPotion = /** @class */ (function (_super) {
         _this._name = name;
         return _this;
     }
+    Object.defineProperty(HealthPotion.prototype, "name", {
+        get: function () {
+            return this._name;
+        },
+        enumerable: false,
+        configurable: true
+    });
     return HealthPotion;
 }(simpleItem));
 var ManaPotion = /** @class */ (function (_super) {
@@ -61,11 +69,32 @@ var ManaPotion = /** @class */ (function (_super) {
         _this._name = name;
         return _this;
     }
+    Object.defineProperty(ManaPotion.prototype, "name", {
+        get: function () {
+            return this._name;
+        },
+        enumerable: false,
+        configurable: true
+    });
     return ManaPotion;
 }(simpleItem));
 var simpleCharacter = /** @class */ (function () {
     function simpleCharacter() {
     }
+    Object.defineProperty(simpleCharacter.prototype, "health", {
+        get: function () {
+            return this._health;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(simpleCharacter.prototype, "name", {
+        get: function () {
+            return this._name;
+        },
+        enumerable: false,
+        configurable: true
+    });
     return simpleCharacter;
 }());
 var Warrior = /** @class */ (function (_super) {
@@ -77,7 +106,7 @@ var Warrior = /** @class */ (function (_super) {
         return _this;
     }
     Warrior.prototype.attack = function () {
-        return "\"Warrior ".concat(this._name, " attacks with ").concat(getrandomInit(15, 25));
+        return "\"Warrior ".concat(this._name, " attacks with ").concat(getrandomInit(15, 25), "\"");
     };
     return Warrior;
 }(simpleCharacter));
@@ -90,7 +119,7 @@ var Mage = /** @class */ (function (_super) {
         return _this;
     }
     Mage.prototype.attack = function () {
-        return "\"Mage ".concat(this._name, " uses a spell on ").concat(getrandomInit(20, 35));
+        return "\"Mage ".concat(this._name, " uses a spell on ").concat(getrandomInit(20, 35), "\"");
     };
     return Mage;
 }(simpleCharacter));
@@ -102,12 +131,15 @@ var Player = /** @class */ (function () {
     Player.prototype.takeDamage = function (damage) {
         var text = '';
         this._health -= damage;
-        text = this._health < 0 ? 'Player is death' : "Player still has ".concat(this._health, " HP");
+        text = this._health < 0 ? 'Player is dead' : "Player still has ".concat(this._health, " HP");
         return text;
     };
     Object.defineProperty(Player.prototype, "health", {
         get: function () {
             return this._health;
+        },
+        set: function (health) {
+            this._health -= health;
         },
         enumerable: false,
         configurable: true
@@ -127,6 +159,20 @@ var Sword = /** @class */ (function () {
         this._damage = getrandomInit(1, 12);
         this._length = getrandomInit(5, 15);
     }
+    Object.defineProperty(Sword.prototype, "name", {
+        get: function () {
+            return this._name;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Sword.prototype, "length", {
+        get: function () {
+            return this._length;
+        },
+        enumerable: false,
+        configurable: true
+    });
     return Sword;
 }());
 var performAttack = function (character) {
@@ -145,3 +191,141 @@ var healthpotion = new HealthPotion('firstpotion');
 var manapotion = new ManaPotion('secondpotion');
 console.log(healthpotion.use());
 console.log(manapotion.use());
+/* ---------------------------------- FRONT ---------------------------------- */
+var componentsarray = ['Players', 'Warriors', 'Mages', 'Swords', 'HealthPotions', 'ManaPotions'];
+var aside = document.querySelector('aside');
+if (aside) {
+    componentsarray.forEach(function (v) {
+        var div = document.createElement('div');
+        var h1 = document.createElement('h1');
+        h1.innerHTML = v;
+        div.classList.add(v);
+        div.appendChild(h1);
+        aside.appendChild(div);
+    });
+}
+var Playersarray = [];
+(_a = document.querySelector('.createaplayer')) === null || _a === void 0 ? void 0 : _a.addEventListener('click', function () {
+    var playername = document.querySelector('.playername');
+    if (playername) {
+        if (!playername.value) {
+            alert('enter a players name');
+            return 0;
+        }
+        else {
+            var Alan = new Player(playername.value);
+            Playersarray = __spreadArray(__spreadArray([], Playersarray, true), [Alan], false);
+            var players = document.querySelector('.Players');
+            if (players) {
+                players.innerHTML += "<span><p>\uD83D\uDD79\uFE0F ".concat(Alan.name, " - ").concat(Alan.health, "</p><button>take damage</button></span>");
+                document.querySelectorAll('.Players>span').forEach(function (v, i) {
+                    var btn = v.querySelector('button');
+                    if (btn) {
+                        btn.addEventListener('click', function () {
+                            Playersarray[i].health = getrandomInit(5, 15);
+                            Playersarray[i].health > 0 ?
+                                v.children[0].innerHTML = "<p>\uD83D\uDD79\uFE0F ".concat(Playersarray[i].name, " - ").concat(Playersarray[i].health, "</p>") :
+                                v.children[0].innerHTML = "<p>\uD83D\uDE1E ".concat(Playersarray[i].name, " is dead</p>");
+                        });
+                    }
+                });
+            }
+            playername.value = '';
+        }
+    }
+});
+var Warriorsarray = [];
+(_b = document.querySelector('.createawarrior')) === null || _b === void 0 ? void 0 : _b.addEventListener('click', function () {
+    var warriorname = document.querySelector('.warriorname');
+    if (warriorname) {
+        if (!warriorname.value) {
+            alert('enter a warriors name');
+            return 0;
+        }
+        else {
+            var Djo = new Warrior(warriorname.value);
+            Warriorsarray = __spreadArray(__spreadArray([], Warriorsarray, true), [Djo], false);
+            var warriors = document.querySelector('.Warriors');
+            if (warriors) {
+                warriors.innerHTML += "<span><p>\uD83D\uDEE1\uFE0F ".concat(Djo.name, " - ").concat(Djo.health, "</p></span>");
+            }
+            warriorname.value = '';
+        }
+    }
+});
+var Magesarray = [];
+(_c = document.querySelector('.createamage')) === null || _c === void 0 ? void 0 : _c.addEventListener('click', function () {
+    var magename = document.querySelector('.magename');
+    if (magename) {
+        if (!magename.value) {
+            alert('enter a mage name');
+            return 0;
+        }
+        else {
+            var Megumin = new Mage(magename.value);
+            Magesarray = __spreadArray(__spreadArray([], Magesarray, true), [Megumin], false);
+            var mages = document.querySelector('.Mages');
+            if (mages) {
+                mages.innerHTML += "<span><p>\uD83E\uDDDD\u200D\u2642\uFE0F ".concat(Megumin.name, " - ").concat(Megumin.health, "</p></span>");
+            }
+            magename.value = '';
+        }
+    }
+});
+var Swordsarray = [];
+(_d = document.querySelector('.createsword')) === null || _d === void 0 ? void 0 : _d.addEventListener('click', function () {
+    var swordname = document.querySelector('.swordname');
+    if (swordname) {
+        if (!swordname.value) {
+            alert('enter a sword name');
+            return 0;
+        }
+        else {
+            var ChunChun = new Sword(swordname.value);
+            Swordsarray = __spreadArray(__spreadArray([], Swordsarray, true), [ChunChun], false);
+            var swords = document.querySelector('.Swords');
+            if (swords) {
+                swords.innerHTML += "<span><p>\u2694\uFE0F ".concat(ChunChun.name, " - ").concat(ChunChun.length, "</p></span>");
+            }
+            swordname.value = '';
+        }
+    }
+});
+var HealthPotionsarray = [];
+(_e = document.querySelector('.createhealth')) === null || _e === void 0 ? void 0 : _e.addEventListener('click', function () {
+    var healthname = document.querySelector('.healthname');
+    if (healthname) {
+        if (!healthname.value) {
+            alert('enter a Health Potion name');
+            return 0;
+        }
+        else {
+            var HP = new HealthPotion(healthname.value);
+            HealthPotionsarray = __spreadArray(__spreadArray([], HealthPotionsarray, true), [HP], false);
+            var healthPotions = document.querySelector('.HealthPotions');
+            if (healthPotions) {
+                healthPotions.innerHTML += "<span><p>\u2764\uFE0F\u200D\uD83E\uDE79 - ".concat(HP.name, "}</p></span>");
+            }
+            healthname.value = '';
+        }
+    }
+});
+var ManaPotionsarray = [];
+(_f = document.querySelector('.createmana')) === null || _f === void 0 ? void 0 : _f.addEventListener('click', function () {
+    var mananame = document.querySelector('.mananame');
+    if (mananame) {
+        if (!mananame.value) {
+            alert('enter a Mana Potion name');
+            return 0;
+        }
+        else {
+            var Mana = new ManaPotion(mananame.value);
+            ManaPotionsarray = __spreadArray(__spreadArray([], ManaPotionsarray, true), [Mana], false);
+            var manaPotions = document.querySelector('.ManaPotions');
+            if (manaPotions) {
+                manaPotions.innerHTML += "<span><p>\u2728 ".concat(Mana.name, "}</p></span>");
+            }
+            mananame.value = '';
+        }
+    }
+});
--- part4/script.ts
+++ part4/script.ts
@@ -7,50 +7,64 @@ interface Weapon {
     _damage: number
 }
 
-interface Inventory{
-    addItem(item: simpleItem):void
-    removeItem(item: simpleItem):void
+interface Inventory {
+    addItem(item: simpleItem): void
+    removeItem(item: simpleItem): void
 }
 
-abstract class simpleItem{
+abstract class simpleItem {
     abstract _name: string
-    use():string{
+    use(): string {
         return 'potion used'
     }
 }
 
-class PlayerInventory implements Inventory{
+class PlayerInventory implements Inventory {
     _pieces: simpleItem[] = []
-    addItem(item: simpleItem):void{
-        this._pieces = [...this._pieces,item]
+    addItem(item: simpleItem): void {
+        this._pieces = [...this._pieces, item]
     }
-    removeItem(item: simpleItem):void{
-        this._pieces = this._pieces.filter(v => v!= item)
+    removeItem(item: simpleItem): void {
+        this._pieces = this._pieces.filter(v => v != item)
     }
 }
 
-
-class HealthPotion extends simpleItem{
+class HealthPotion extends simpleItem {
     _name: string
     constructor(name: string) {
         super()
         this._name = name
     }
+
+    get name():string{
+        return this._name
+    }
 }
 
-class ManaPotion extends simpleItem{
+class ManaPotion extends simpleItem {
     _name: string
     constructor(name: string) {
         super()
         this._name = name
+    }
+    get name(): string {
+        return this._name
     }
 }
 
 abstract class simpleCharacter {
-    abstract _name: string
-    abstract _health: number
+    protected _name: string
+    protected _health: number
 
     abstract attack(): string
+
+    get health(): number {
+        return this._health
+    }
+
+    get name(): string {
+        return this._name
+    }
 }
 
 class Warrior extends simpleCharacter {
@@ -62,9 +76,10 @@ class Warrior extends simpleCharacter {
         this._health = 100
     }
     attack(): string {
-        return `"Warrior ${this._name} attacks with ${getrandomInit(15, 25)}`
+        return `"Warrior ${this._name} attacks with ${getrandomInit(15, 25)}"`
     }
 }
+
 class Mage extends simpleCharacter {
     _name: string
     _health: number
@@ -74,13 +89,13 @@ class Mage extends simpleCharacter {
         this._health = 100
     }
     attack(): string {
-        return `"Mage ${this._name} uses a spell on ${getrandomInit(20, 35)}`
+        return `"Mage ${this._name} uses a spell on ${getrandomInit(20, 35)}"`
     }
 }
 
 class Player {
-   private _name: string
-   private _health: number
+    private _name: string
+    private _health: number
 
     constructor(name: string) {
         this._name = name
@@ -90,14 +105,17 @@ class Player {
     takeDamage(damage: number): string {
         let text = ''
         this._health -= damage
-        text = this._health < 0 ? 'Player is death' : `Player still has ${this._health} HP`
+        text = this._health < 0 ? 'Player is dead' : `Player still has ${this._health} HP`
         return text
     }
 
-    get health():number{
+    get health(): number {
         return this._health
     }
-    get name():string{
+    set health(health: number) {
+        this._health -= health
+    }
+    get name(): string {
         return this._name
     }
 }
@@ -110,6 +128,13 @@ class Sword implements Weapon {
         this._name = name
         this._damage = getrandomInit(1, 12)
         this._length = getrandomInit(5, 15)
+    }
+    get name():string{
+        return this._name
+    }
+
+    get length():number{
+        return this._length
     }
 }
 
@@ -134,3 +159,153 @@ const manapotion = new ManaPotion('secondpotion')
 
 console.log(healthpotion.use())
 console.log(manapotion.use())
+
+/* ---------------------------------- FRONT ---------------------------------- */
+
+const componentsarray: string[] = ['Players', 'Warriors', 'Mages', 'Swords', 'HealthPotions', 'ManaPotions']
+
+const aside = document.querySelector('aside')
+
+if (aside) {
+    componentsarray.forEach(v => {
+        const div = document.createElement('div')
+        const h1 = document.createElement('h1')
+        h1.innerHTML = v
+        div.classList.add(v)
+        div.appendChild(h1)
+        aside.appendChild(div)
+    })
+}
+
+let Playersarray: Player[] = []
+
+document.querySelector('.createaplayer')?.addEventListener('click', () => {
+    const playername: HTMLInputElement | null = document.querySelector('.playername')
+    if (playername) {
+        if (!playername.value) {
+            alert('enter a players name')
+            return 0
+        } else {
+            
+            const Alan = new Player(playername.value)
+            Playersarray = [...Playersarray, Alan]
+            const players = document.querySelector('.Players')
+            if (players) {
+                players.innerHTML += `<span><p>🕹️ ${Alan.name} - ${Alan.health}</p><button>take damage</button></span>`
+
+                document.querySelectorAll('.Players>span').forEach((v, i) => {
+                    const btn = v.querySelector('button');
+                    if (btn) {
+                        btn.addEventListener('click', () => {
+                            Playersarray[i].health = getrandomInit(5, 15)
+                            Playersarray[i].health > 0 ?
+                                v.children[0].innerHTML = `<p>🕹️ ${Playersarray[i].name} - ${Playersarray[i].health}</p>` :
+                                v.children[0].innerHTML = `<p>😞 ${Playersarray[i].name} is dead</p>`
+                        });
+                    }
+                });
+            }
+            playername.value = ''
+        }
+    }
+})
+
+let Warriorsarray: Warrior[] = []
+
+document.querySelector('.createawarrior')?.addEventListener('click', () => {
+    const warriorname: HTMLInputElement | null = document.querySelector('.warriorname')
+    if (warriorname) {
+        if (!warriorname.value) {
+            alert('enter a warriors name')
+            return 0
+        } else {
+            const Djo = new Warrior(warriorname.value)
+            Warriorsarray = [...Warriorsarray, Djo]
+            const warriors = document.querySelector('.Warriors')
+            if (warriors) {
+                warriors.innerHTML += `<span><p>🛡️ ${Djo.name} - ${Djo.health}</p></span>`
+            }
+            warriorname.value = ''
+        }
+    }
+})
+
+let Magesarray: Mage[] = []
+
+document.querySelector('.createamage')?.addEventListener('click', () => {
+    const magename: HTMLInputElement | null = document.querySelector('.magename')
+    if (magename) {
+        if (!magename.value) {
+            alert('enter a mage name')
+            return 0
+        } else {
+            const Megumin = new Mage(magename.value)
+            Magesarray = [...Magesarray, Megumin]
+            const mages = document.querySelector('.Mages')
+            if (mages) {
+                mages.innerHTML += `<span><p>🧝‍♂️ ${Megumin.name} - ${Megumin.health}</p></span>`
+            }
+            magename.value = ''
+        }
+    }
+})
+
+let Swordsarray: Sword[] = []
+
+document.querySelector('.createsword')?.addEventListener('click', () => {
+    const swordname: HTMLInputElement | null = document.querySelector('.swordname')
+    if (swordname) {
+        if (!swordname.value) {
+            alert('enter a sword name')
+            return 0
+        } else {
+            const ChunChun = new Sword(swordname.value)
+            Swordsarray = [...Swordsarray, ChunChun]
+            const swords = document.querySelector('.Swords')
+            if (swords) {
+                swords.innerHTML += `<span><p>⚔️ ${ChunChun.name} - ${ChunChun.length}</p></span>`
+            }
+            swordname.value = ''
+        }
+    }
+})
+
+let HealthPotionsarray: HealthPotion[] = []
+
+document.querySelector('.createhealth')?.addEventListener('click', () => {
+    const healthname: HTMLInputElement | null = document.querySelector('.healthname')
+    if (healthname) {
+        if (!healthname.value) {
+            alert('enter a Health Potion name')
+            return 0
+        } else {
+            const HP = new HealthPotion(healthname.value)
+            HealthPotionsarray = [...HealthPotionsarray, HP]
+            const healthPotions = document.querySelector('.HealthPotions')
+            if (healthPotions) {
+                healthPotions.innerHTML += `<span><p>❤️‍🩹 - ${HP.name}}</p></span>`
+            }
+            healthname.value = ''
+        }
+    }
+})
+
+let ManaPotionsarray: ManaPotion[] = []
+
+document.querySelector('.createmana')?.addEventListener('click', () => {
+    const mananame: HTMLInputElement | null = document.querySelector('.mananame')
+    if (mananame) {
+        if (!mananame.value) {
+            alert('enter a Mana Potion name')
+            return 0
+        } else {
+            const Mana = new ManaPotion(mananame.value)
+            ManaPotionsarray = [...ManaPotionsarray, Mana]
+            const manaPotions = document.querySelector('.ManaPotions')
+            if (manaPotions) {
+                manaPotions.innerHTML += `<span><p>✨ ${Mana.name}}</p></span>`
+            }
+            mananame.value = ''
+        }
+    }
+})
